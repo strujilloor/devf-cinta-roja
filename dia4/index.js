420b2a93b2d931ac10fbec19bec0ac485e64aea2
Luis Stiven Trujillo Ortiz <luis.trujilloor@campusucc.edu.co>
2020-01-25 10:28:47 -0500

    dia4: request más legible

--- dia4/index.js
+++ dia4/index.js
@@ -14,11 +14,17 @@ class Pokemon {
 
 const getPokemon = (pokemon) => {
     return new Promise((resolve, reject) => {
-        request(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`, (error, response, body) => {
+        const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+        request( URL, (error, response, body) => {
             console.log(`Status code ----> ${response.statusCode}`);
             if( response.statusCode === 200 ) {
                 const resPoke = JSON.parse(body)
-                const pokemon = new Pokemon(resPoke.id, resPoke.name, resPoke.sprites.front_default, resPoke.moves);
+                const pokemon = new Pokemon(
+                    resPoke.id, 
+                    resPoke.name, 
+                    resPoke.sprites.front_default, 
+                    resPoke.moves
+                );
                 resolve(pokemon);
             } else {
                 reject(error);
